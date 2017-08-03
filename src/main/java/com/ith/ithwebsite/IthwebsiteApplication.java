3f579c6c4fea30e2b961ffa23806b4a2d9161c16
Nathan Banks <nathan.banksd@gmail.com>
2017-08-03 12:24:37 -0700

    start stuff

---
+++ src/main/java/com/ith/ithwebsite/IthwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.ith.ithwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IthwebsiteApplication.class, args);
+	}
+}
